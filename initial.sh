1916c67c054c181f112566d9c4f164d221e28bae
YolandaSunYS <62234532+YolandaSunYS@users.noreply.github.com>
2024-10-21 13:21:34 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -2,3 +2,7 @@
 echo "initialize $1"
 echo "FUNCTION_CONTEXT is $FUNCTION_CONTEXT"
 echo "master branch"
+
+echo "initialize $1" > $2
+echo "FUNCTION_CONTEXT is $FUNCTION_CONTEXT" >> $2
+echo "master branch" >> $2
